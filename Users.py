63fa40e38c6f74e05a9152ec0124ae4f4a8a2571
Artpukh <artemytemik@gmail.com>
2022-03-25 15:18:27 +0300

    the first by Artyom

---
+++ Users.py
@@ -0,0 +1,14 @@
+import datetime
+import sqlalchemy
+from db_session import SqlAlchemyBase
+from sqlalchemy import orm
+
+
+class User(SqlAlchemyBase):
+    __tablename__ = 'users'
+
+    id = sqlalchemy.Column(sqlalchemy.Integer,
+                           primary_key=True, autoincrement=True)
+    nickname = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+
+    words = orm.relation("Words", back_populates='user')
---
+++ main.py
@@ -0,0 +1,12 @@
+import sqlalchemy as sa
+from db_session import *
+
+
+def main():
+    global_init("accounts.db")
+
+
+if __name__ == '__main__':
+    main()
+
+
