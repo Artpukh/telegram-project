0b7d98c341665dbc88e713ee1d51825ecbdb29ca
Artpukh <artemytemik@gmail.com>
2022-03-30 21:37:49 +0300

    the second by Artyom

--- main.py
+++ main.py
@@ -1,9 +1,46 @@
 import sqlalchemy as sa
 from db_session import *
+import logging
+from telegram.ext import Updater, MessageHandler, Filters, CommandHandler
+
+
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.DEBUG)
+
+logger = logging.getLogger(__name__)
+
+TOKEN = '5296805065:AAEwF8fUJQ36bOxG6UdQIbwf4zwcjDxHE0g'
+
+
+def start(update, context):
+    pass
+
+
+def help(update, context):
+    update.message.reply_text(
+        "Привет! Это бот-переводчик!\n"
+        "Мои возможномти:\n"
+        "/reg - мы настоятельно рекомендуем вам зарегестрироваться, для регистрации нужно только придумать никнейм;"
+        "/enter - вход в учётную запись;"
+        "/translate - перевод слова или выражения;\n"
+        "/translate_file - перевод содержимого файла, возвращает файл с переводом;\n"
+        "/translate_from_file - бот с выбранной периодчностью будет давать перевод слова из отправленного файла;\n"
+        "/game - игра: бот загадывает слово, пользователь даёт перевод;"
+        )
 
 
 def main():
     global_init("accounts.db")
+
+    updater = Updater(TOKEN)
+
+    dp = updater.dispatcher
+    dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(CommandHandler("help", help))
+
+    updater.start_polling()
+
+    updater.idle()
 
 
 if __name__ == '__main__':
